21ba6f926a1e3791f768e387a48d4f9f296c407b
rekarachmadi99 <reka.rachmadi@gmail.com>
2024-11-28 20:12:18 +0700

    update interval

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,8 @@
 class Config {
   constructor() {
-    this.ipCheckURL = 'https://ipinfo.io/json';
-    this.wssList = ['proxy2.wynd.network:4444', 'proxy2.wynd.network:4650'];
-    this.retryInterval = 20000;
+    this.ipCheckURL = "https://ipinfo.io/json";
+    this.wssList = ["proxy2.wynd.network:4444", "proxy2.wynd.network:4650"];
+    this.retryInterval = 1000;
     this.wssHost =
       this.wssList[Math.floor(Math.random() * this.wssList.length)];
   }
