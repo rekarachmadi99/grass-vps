28717cd2548bfc527426fe21891063432d04a712
Reka Rachmadi Apriansyah <107926896+rekarachmadi99@users.noreply.github.com>
2024-11-29 09:44:06 +0700

    v1

--- src/Config.js
+++ src/Config.js
@@ -2,7 +2,7 @@ class Config {
   constructor() {
     this.ipCheckURL = "https://ipinfo.io/json";
     this.wssList = ["proxy2.wynd.network:4444", "proxy2.wynd.network:4650"];
-    this.retryInterval = 1000;
+    this.retryInterval = 900;
     this.wssHost =
       this.wssList[Math.floor(Math.random() * this.wssList.length)];
   }
